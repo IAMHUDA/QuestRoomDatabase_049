6980592650872aa2cb327b4f2f5309ca2c821399
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-15 23:08:43 +0700

    menambah fun mengambil data mahasiswa berdasarkan nim

--- app/src/main/java/com/example/pertemuan10roomlocaldb/repository/RepositoryMhs.kt
+++ app/src/main/java/com/example/pertemuan10roomlocaldb/repository/RepositoryMhs.kt
@@ -1,7 +1,10 @@
 package com.example.pertemuan10roomlocaldb.repository
 
 import com.example.pertemuan10roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 interface RepositoryMhs {
     suspend fun insertMhs(mahasiswa: Mahasiswa)
+    fun getAllMhs(): Flow<List<Mahasiswa>>
+    fun getMhs(nim: String): Flow<Mahasiswa>
 }
