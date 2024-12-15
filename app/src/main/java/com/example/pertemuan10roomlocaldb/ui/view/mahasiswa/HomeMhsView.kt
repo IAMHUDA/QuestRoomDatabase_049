1128c416e7696d24fed27cbb037a588c6c11a149
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-16 00:24:16 +0700

    menambah bodyhomemhsview

--- app/src/main/java/com/example/pertemuan10roomlocaldb/ui/view/mahasiswa/HomeMhsView.kt
+++ app/src/main/java/com/example/pertemuan10roomlocaldb/ui/view/mahasiswa/HomeMhsView.kt
@@ -1,19 +1,32 @@
 package com.example.pertemuan10roomlocaldb.ui.view.mahasiswa
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.pertemuan10roomlocaldb.ui.costumwidget.TopAppBar
 import com.example.pertemuan10roomlocaldb.ui.viewmodel.HomeMhsViewModel
+import com.example.pertemuan10roomlocaldb.ui.viewmodel.HomeUiState
 import com.example.pertemuan10roomlocaldb.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeMhsView(
@@ -50,5 +63,62 @@ fun HomeMhsView(
             },
             modifier = Modifier.padding(innerPadding)
         )
+    }
+}
+
+@Composable
+fun BodyHomeMhsView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() } //Snackbar stste
+    when {
+        homeUiState.isLoading -> {
+            // Menampilkan indikator loading
+            Box (modifier= modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center)
+            {
+                CircularProgressIndicator()
+            }
+        }
+
+        homeUiState.isError -> {
+            //menampilkan pesan error
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let{message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)  //menampilkan snackbar
+                    }
+                }
+            }
+        }
+
+        homeUiState.listMhs.isEmpty() -> {
+            //menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data mahasiswa.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else -> {
+            //Menampilkan daftar mahasiswa
+            ListMahasiswa(
+                listMhs = homeUiState.listMhs,
+                onClick = {
+                    onClick(it)
+                    println(it)
+                },
+                modifier = Modifier.padding(top = 100.dp)
+            )
+        }
     }
 }
