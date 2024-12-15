159d5f5c3b15b6db3c60a5c01e9968ca30d9019e
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-15 23:24:49 +0700

    menambah class homemhsviewmodel

---
+++ app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.pertemuan10roomlocaldb.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan10roomlocaldb.repository.RepositoryMhs
+
+class HomeMhsViewModel (
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel()
+{
+
+}
