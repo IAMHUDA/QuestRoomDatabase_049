d346271613690f30baab36deaeeb987a76e5cc4a
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-15 23:36:44 +0700

    menambah val homeUIState

--- app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
@@ -1,11 +1,23 @@
 package com.example.pertemuan10roomlocaldb.ui.viewmodel
 
+import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
 import com.example.pertemuan10roomlocaldb.repository.RepositoryMhs
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.filterNot
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
-) : ViewModel()
-{
+) : ViewModel() {
+    val homeUIState: StateFlow<HomeUiState> = repositoryMhs.getAllMhs()
+        .filterNotNull()
+        .map {
+            HomeUiState(
+                listMhs = it.toList(),
+                isLoading = false,
+            )
+        }
 
 }
