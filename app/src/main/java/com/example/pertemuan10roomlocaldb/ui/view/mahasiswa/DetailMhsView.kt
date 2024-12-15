ed870ee1bbc8a6d050c2b5675dc11d268a0a5914
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-16 00:35:22 +0700

    menambah componentdetail mhs

--- app/src/main/java/com/example/pertemuan10roomlocaldb/ui/view/mahasiswa/DetailMhsView.kt
+++ app/src/main/java/com/example/pertemuan10roomlocaldb/ui/view/mahasiswa/DetailMhsView.kt
@@ -25,6 +25,8 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -163,5 +165,28 @@ fun ItemDetailMhs(
             ComponentDetailMhs(judul = "Angkatan", isinya = mahasiswa.angkatan)
 
         }
+    }
+}
+
+@Composable
+fun ComponentDetailMhs(
+    modifier: Modifier =Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column(modifier =modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start)
+    {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 }
