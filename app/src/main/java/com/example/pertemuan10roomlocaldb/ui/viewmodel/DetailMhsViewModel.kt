83d1af471d4954c34f06a553fa169226f433b7b5
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-15 23:58:42 +0700

    menambah data class DetailUiState

--- app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/DetailMhsViewModel.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.time.delay
 
 class DetailMhsViewModel (
@@ -47,4 +48,25 @@ class DetailMhsViewModel (
             initialValue =
             DetailUiState(isLoading = true)
         )
+    fun deleteMhs(){
+        detailUiState.value.detailUiEvent.toMahasiswaEntity().let {
+            viewModelScope.launch {
+                repositoryMhs.deleteMhs(it)
+            }
+        }
+    }
+}
+
+data class DetailUiState(
+    val detailUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)
+{
+    val isUIEventEmpty: Boolean
+        get() = detailUiEvent == MahasiswaEvent()
+
+    val isUiEventNotEmpty: Boolean
+        get() = detailUiEvent != MahasiswaEvent()
 }
