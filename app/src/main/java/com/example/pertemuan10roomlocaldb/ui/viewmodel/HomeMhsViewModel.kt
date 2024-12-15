d690a4ba8628f1697f531e8ae7ba046c2419cb84
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-15 23:37:57 +0700

    menambah on start

--- app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/HomeMhsViewModel.kt
@@ -3,10 +3,12 @@ package com.example.pertemuan10roomlocaldb.ui.viewmodel
 import androidx.compose.runtime.State
 import androidx.lifecycle.ViewModel
 import com.example.pertemuan10roomlocaldb.repository.RepositoryMhs
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.filterNot
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
 
 class HomeMhsViewModel (
     private val repositoryMhs: RepositoryMhs
@@ -19,5 +21,8 @@ class HomeMhsViewModel (
                 isLoading = false,
             )
         }
-
+        .onStart {
+            emit(HomeUiState(isLoading = true))
+            delay(900)
+        }
 }
