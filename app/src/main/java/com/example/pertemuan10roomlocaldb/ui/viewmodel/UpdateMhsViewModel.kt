03fb848ad665c45e4bd10d8dbe5dcd4e4d4d91a7
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-16 00:10:40 +0700

    menambah fun updateState

--- app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,12 +1,36 @@
 package com.example.pertemuan10roomlocaldb.ui.viewmodel
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan10roomlocaldb.repository.RepositoryMhs
+import com.example.pertemuan10roomlocaldb.ui.navigation.DestinasiUpdate
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class UpdateMhsViewModel(
     savedStateHandle: SavedStateHandle,
     private val repositoryMhs: RepositoryMhs
 ) : ViewModel() {
+    var updateUiState by mutableStateOf(MhsUIState())
+        private set
+
+    private val _nim: String = checkNotNull(savedStateHandle[DestinasiUpdate.NIM])
+
+    init {
+        viewModelScope.launch {
+            updateUiState = repositoryMhs.getMhs(_nim)
+                .filterNotNull()
+                .first()
+                .toUIStateMhs()
+        }
+    }
+    fun updateState(mahasiswaEvent: MahasiswaEvent){
+        updateUiState = updateUiState.copy(
+            mahasiswaEvent = mahasiswaEvent,
+        )
+    }
 
 }
