95216953d317b73f7648123fb1b92eac6fcf589a
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-15 22:55:56 +0700

    menambahkan dao get mahasiswa

--- app/src/main/java/com/example/pertemuan10roomlocaldb/data/dao/MahasiswaDao.kt
+++ app/src/main/java/com/example/pertemuan10roomlocaldb/data/dao/MahasiswaDao.kt
@@ -1,11 +1,19 @@
 package com.example.pertemuan10roomlocaldb.data.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
 import com.example.pertemuan10roomlocaldb.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface MahasiswaDao {
     @Insert
     suspend fun insertMahasiswa(mahasiswa: Mahasiswa)
+
+    //get Mahasiswa
+    @Query("SELECT * FROM mahasiswa WHERE nim = :nim")
+    fun getMahasiswa(nim: String): Flow<Mahasiswa>
 }
