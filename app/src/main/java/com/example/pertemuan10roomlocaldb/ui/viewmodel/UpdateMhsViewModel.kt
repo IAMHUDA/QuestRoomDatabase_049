681972f6394227574a894cc30c15c2e1947bff68
IAMHUDA <hudamiftahul362@gmail.com>
2024-12-16 00:09:16 +0700

    menambah save data handel

--- app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/pertemuan10roomlocaldb/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,4 +1,12 @@
 package com.example.pertemuan10roomlocaldb.ui.viewmodel
 
-class UpdateMhsViewModel {
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.pertemuan10roomlocaldb.repository.RepositoryMhs
+
+class UpdateMhsViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositoryMhs: RepositoryMhs
+) : ViewModel() {
+
 }
